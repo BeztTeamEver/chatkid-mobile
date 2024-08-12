eb7881799e5d52239081d40a84247dfc8271f067
Dao Thien Binh <daothienbinh@cigro.io>
2024-08-12 21:48:30 +0700

    DELETE - duplicate functiobn

--- lib/pages/controller/todo_page/todo_home_store.dart
+++ lib/pages/controller/todo_page/todo_home_store.dart
@@ -236,14 +236,6 @@ class TodoHomeStore extends GetxController {
     targets.removeWhere((element) => element.id == target.id);
   }
 
-  updateTarget(TargetModel target) {
-    final index = targets.indexWhere((element) => element.id == target.id);
-    if (index != -1) {
-      targets[index] = target;
-    }
-    targets.refresh();
-  }
-
   setTask(String id) {
     currentTask.value = id;
   }
